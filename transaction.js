c296d091ae22d758a13d84566fe1b9de34e71e7d
nikitkhakholia <khakholia.nk@gmail.com>
2022-02-07 14:12:13 +0530

    angul;ar

---
+++ transaction.js
@@ -0,0 +1,37 @@
+var app = angular.module('myApp',[]);
+app.controller('transactionCtrl',function($scope, $http)
+{
+    $http.get("https://khakholia.github.io/angular/transaction.json")
+    .then(function(response)
+    {
+        $scope.myData = response.data.data;
+        $scope.rowlimit = response.data.length();
+        // $scope.orderByMe = function(x) 
+        // {
+        //     $scope.myOrderBy = x;
+        // }
+    });
+
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+      }
+    
+});
+
+app.filter("myfilter",function()
+{
+    return function(input)
+    {
+        return input.substring(0,1).toUpperCase() + input.substring(1);
+    }
+});
+
+
+app.filter('myNameFilter',function()
+{
+    return function(input)
+    {
+        return ' ' + input;
+    }
+});
+
